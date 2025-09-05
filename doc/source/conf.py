5ed981264dc2b03c35a8bfdae664943fc898ea33
Rachel Clune <rachel.clune@omsf.io>
2025-09-05 16:33:46 -0700

    Setting up a home for the external documentation. Changed the theme from RtD to Furo and adjusted to match RC branding.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -6,9 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'rf_diffusion'
-copyright = '2024, Baker Lab'
-author = 'Baker Lab'
+project = 'RFdiffusion2'
+copyright = '2025, Institute for Protein Design, University of Washington'
+author = 'Woody Ahern, Jason Yim, Doug Tischer, Saman Salike, Seth M. Woodbury, Donghyo Kim, Indrek Kalvet, Yakov Kipnis, Brian Coventry, Han Raut Altae-Tran, Magnus Bauer, Regina Barzilay, Tommi S. Jaakkola, Rohith Krishna, David Baker'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -17,11 +17,13 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
     'sphinx.ext.autodoc',
-    'sphinx_rtd_theme',
     'sphinx.ext.napoleon',
     'sphinx_mdinclude',
+    #'myst_parser', # to use markdown instead of ReST'
 
 ]
+
+#myst_enable_extensions = ["colon_fence"] # see https://mystmd.org/guide/syntax-overview for more information
 
 suppress_warnings = [
     'app.add_node',
@@ -66,5 +68,32 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 html_static_path = ['_static']
+
+
+html_theme_options = {
+    "sidebar_hide_name":False,
+    "top_of_page_buttons": ["edit"],
+    ""
+    #"announcement": "<em>THIS DOCUMENTATION IS CURRENTLY UNDER CONSTRUCTION</em>",
+    "light_css_variables": {
+        "color-brand-primary": "#F68A33", # Rosetta Teal
+        "color-brand-content": "#37939B", # Rosetta Orange
+        "color-admonition-background": "#FB35D6", # Rosetta light orange
+        "font-stack": "Open Sans, sans-serif",
+        "font-stack--headings": "Open Sans, sans-serif",
+        "color-background-hover": "#DCE8E8ff",
+        "color-announcement-background" : "#F68A33dd",
+        "color-announcement-text": "#070707",
+        "color-brand-visited": "#37939B",
+        },
+    "dark_css_variables": {
+        "color-brand-primary": "#37939B", # Rosetta teal
+        "color-brand-content": "#F68A33", # Rosetta orange
+        "color-admonition-background": "#FB35D6", # Rosetta light orange
+        "font-stack": "Open Sans, sans-serif",
+        "font-stack--headings": "Open Sans, sans-serif",
+        "color-brand-visited": "#37939B",
+        }
+    }
--- doc/source/index.rst
+++ doc/source/index.rst
@@ -3,8 +3,8 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to rf_diffusion's documentation!
-========================================
+Welcome to the Official Documentation for RFdiffusion2!
+=======================================================
 
 .. toctree::
    :maxdepth: 2
